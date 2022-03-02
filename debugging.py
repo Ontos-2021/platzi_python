358862206889ee4927327b1041a27e67180e8247
Ontos <josemiguel.mercado17@gmail.com>
2022-03-02 00:08:06 -0300

    Poniendo a prueba el manejo de excepciones

--- debugging.py
+++ debugging.py
@@ -1,11 +1,22 @@
 def divisors(num):
-    divisors = [i for i in range(1, num + 1) if num % i == 0]
-    return divisors
+    try:
+        if num == 0:
+            raise ValueError("Debes ingresar un número positivo, distinto de 0")
+        if num < 0:
+            raise ValueError("Debes ingresar un número positivo")
+        divisors = [i for i in range(1, num + 1) if num % i == 0]
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return "No se pudo calcular los divisores"
 
 def run():
-    num = int(input("Ingrese un número: "))
-    print(divisors(num))
-    print("El programa finalizó")
+    try:
+        num = int(input("Ingrese un número: "))
+        print(divisors(num))
+        print("El programa finalizó correctamente")
+    except ValueError:
+        print("Debes ingresar un número")
 
 if __name__ == '__main__':
     run()
